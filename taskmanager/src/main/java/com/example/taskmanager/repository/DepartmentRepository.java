33d780393c6c7ec23524a926834d3f76247a7b24
AverageEnj0yer <nodepresso@gmail.com>
2025-02-16 12:16:52 +0200

    Final changes

--- taskmanager/src/main/java/com/example/taskmanager/repository/DepartmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.taskmanager.repository;
-
-import com.example.taskmanager.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Integer> {
-    Department findByName(String name);
-}
